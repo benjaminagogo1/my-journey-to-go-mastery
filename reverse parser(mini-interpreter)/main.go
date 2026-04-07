3c678dae7f60659b39035e1283265193226abdb9
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-04-07 01:11:54 +0100

    my-journey-to-go-mastery

--- reverse parser(mini-interpreter)/main.go
+++ reverse parser(mini-interpreter)/main.go
@@ -14,7 +14,7 @@ var val int
 func main()  {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	me := 0
+	var stack [] int
 	
 	
 
@@ -41,21 +41,50 @@ func main()  {
 		}
 		
 
-		for i := 0; i < len(te); i++ {
-			c := te[i]
+		for i, r := range ret {
+			if r == "-" || r == "+" || r == "*" || r == "/" {
 
-			if c < '0' || c > '9' {
-				fmt.Println("invalid decimal")
-				break
+
+				if len(stack) < 2 {
+					fmt.Println("Error: Not enough input.")
+					continue
+				} else {
+					b := r[i]
+					num := 0
+					for i := 0; i < len(r); i++ {
+						if b != '0' && b != '1' {
+							fmt.Println("Error: invalid binary input")
+							break
+						}
+						num = num*2 + int(b - '0')
+					}
+					fmt.Println(num)
+				}
+				right := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+
+				left := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+
+				var result int 
+				switch r {
+				case "+":
+					result := left + right
+					fmt.Println(result)
+				case "-":
+					result := left - right
+					fmt.Println(result)
+
+				case "*":
+					result := left * right
+					fmt.Println(result)
+				case "/":
+					result := left / right
+					fmt.Println(result)
+				}
+				stack = append(stack, result)
 			}
-			me = me * 10 + int(c - '0')
 		}
-		fmt.Println(me)
-
-		c := te[i]
-		if c != 0 && c != 1 {}
-		
-
 
 	}
 
@@ -63,13 +92,3 @@ func main()  {
 
 
 
-for i := 0; i < len(te); i++ {
-	c := te[i]
-
-	switch {
-	case c < 0 || c > 9:
-		fmt.Println("invalid decimal")
-
-	
-	}
-}
