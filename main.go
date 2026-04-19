e1e9e5b216bfacbb208443906a9a74d6606fc4e2
botete <josephbenjamin046@gmail.com>
2026-04-19 06:00:49 +0100

    my-journey-to-go-mastery

--- main.go
+++ main.go
@@ -1,220 +1,193 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+)
 
-// func main() {
-// 	s := "255"
-// 	num := 0
-// 	for i := 0; i < len(s); i++ {
-// 		c := s[i]
-// 		value := 0
-// 		if c != '0' && c != '1' {
-// 			value = int(c - '0')
-// 		}
-// 		if c >= 'a' && c <= 'f' {
-// 			value = int(c-'a') + 10
-// 		} else if c >= 'A' && c <= 'F' {
-// 			value = int(c-'A') + 10
-// 		}
-// 		num = num*10 + value
+func main() {
+	s := "255"
+	num := 0
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		value := 0
+		if c != '0' && c != '1' {
+			value = int(c - '0')
+		}
+		if c >= 'a' && c <= 'f' {
+			value = int(c-'a') + 10
+		} else if c >= 'A' && c <= 'F' {
+			value = int(c-'A') + 10
+		}
+		num = num*10 + value
 
-// 	}
+	}
 
-// 	fmt.Println(num)
+	fmt.Println(num)
+
+}
+
+func main() {
+	num := 255
+	base := 2
+
+	var result string
+
+	for num > 0 {
+		bit := num % base
+		result = fmt.Sprint(bit) + result
+		num = num / base
+	}
+
+// 	fmt.Println(result)
+// }
+
+package main
+
+import (
+	"fmt"
+	// "strings"
+)
+
+func main() {
+	var x [5]float64
+	x[0] = 98
+	x[1] = 93
+	x[2] = 77
+	x[3] = 82
+	x[4] = 83
+
+	var total float64 = 0
+	for i := 0; i < len(x); i++  {
+		total += x[i]
+	}
+	fmt.Println(total/float64(len(x)))
+}
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func hew(s []string) string {
+	var result []string
+	for i, words := range s {
+		if s[i] == "(cap)" && i > 0 {
+			words = result[i-1]
+			result[i-1] = strings.ToUpper(string(words[0])) + strings.ToLower(words[1:])
+			continue
+		}
+		result = append(result, words)
+	}
+	return strings.Join(result, " ")
+}
+
+func main() {
+	fmt.Println(hew([]string{"hello", "world", "welcome", "(cap)", "golang"}))
+}
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func capitalize(word string) string {
+	if len(word) == 0 {
+		return word
+	}
+	return strings.ToUpper(string(word[0])) + strings.ToLower(word[1:])
+
+}
+
+func hew(s []string) string {
+	result := []string{}
+
+	for _, token := range s {
+
+		switch token {
+		case "(cap)":
+			if len(result) > 0 {
+				result[len(result)-1] = capitalize(result[len(result)-1])
+			}
+
+		default:
+			result = append(result, token)
+		}
+	}
+
+	return strings.Join(result, " ")
+}
+
+func main() {
+	fmt.Println(hew([]string{"hello", "world", "welcome", "(cap)", "golang"}))
+}
+
+func singleUp(s string) string {
+	words := strings.Fields(s)
+	for i := 0; i < len(words); i++ {
+		if words[i] == "(up)" && i > 0 {
+			words[i-1] = strings.ToUpper(words[i-1])
+			words = append(words[:i], words[i+1:]...)
+		}
+	}
+	r := strings.Join(words, " ")
+	r = strings.ReplaceAll(r, " !", "!")
+	return r
+}
+
+func uppcase(s string) string {
+	words := strings.Fields(s)
+
+	for i := 0; i < len(words)-1; i++ {
+		switch words[i] {
+		case "(up,":
+			words[i+1] = strings.Trim(words[i+1], ")")
+			num, err := strconv.Atoi(words[i+1])
+			if err != nil {
+				fmt.Println(err)
+			}
+			for j := 1; j <= num; j++ {
+				words[i-j] = strings.ToUpper(words[i-j])
+			}
+			words = append(words[:i], words[i+2:]...)
+			i--
+		}
+	}
+	return strings.Join(words, " ")
 
 // }
 
-// func main() {
-// 	num := 255
-// 	base := 2
+func main() {
+	fmt.Println(uppcase("This is so exciting (up, 2)"))
+}
 
-// 	var result string
+package main
 
-// 	for num > 0 {
-// 		bit := num % base
-// 		result = fmt.Sprint(bit) + result
-// 		num = num / base
-// 	}
+import (
+	"fmt"
+	"strings"
+	"unicode"
+	// "strings"
+	// "unicode"
+)
 
-// // 	fmt.Println(result)
-// // }
+func ma(s string) string {
+	c := []rune(s)
+	var result []rune
 
-// package main
-
-// import (
-// 	"fmt"
-// 	// "strings"
-// )
-
-// func main() {
-// 	var x [5]float64
-// 	x[0] = 98
-// 	x[1] = 93
-// 	x[2] = 77
-// 	x[3] = 82
-// 	x[4] = 83
-
-// 	var total float64 = 0
-// 	for i := 0; i < len(x); i++  {
-// 		total += x[i]
-// 	}
-// 	fmt.Println(total/float64(len(x)))
+	for i := 0; i < len(c); i++ {
+		if unicode.IsLetter(c[i]) || unicode.IsSpace(c[i]) {
+			result = append(result, c[i])
+		}
+	}
+	r := strings.Join(strings.Fields(string(result)), " ")
+	return r
 // }
 
-// package main
-
-// import (
-// 	"fmt"
-// 	"strconv"
-// 	"strings"
-// )
-
-// func hew(s []string) string {
-// 	var result []string
-// 	for i, words := range s {
-// 		if s[i] == "(cap)" && i > 0 {
-// 			words = result[i-1]
-// 			result[i-1] = strings.ToUpper(string(words[0])) + strings.ToLower(words[1:])
-// 			continue
-// 		}
-// 		result = append(result, words)
-// 	}
-// 	return strings.Join(result, " ")
-// }
-
-// func main() {
-// 	fmt.Println(hew([]string{"hello", "world", "welcome", "(cap)", "golang"}))
-// }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"strings"
-// )
-
-// func capitalize(word string) string {
-// 	if len(word) == 0 {
-// 		return word
-// 	}
-// 	return strings.ToUpper(string(word[0])) + strings.ToLower(word[1:])
-
-// }
-
-// func hew(s []string) string {
-// 	result := []string{}
-
-// 	for _, token := range s {
-
-// 		switch token {
-// 		case "(cap)":
-// 			if len(result) > 0 {
-// 				result[len(result)-1] = capitalize(result[len(result)-1])
-// 			}
-
-// 		default:
-// 			result = append(result, token)
-// 		}
-// 	}
-
-// 	return strings.Join(result, " ")
-// }
-
-// func main() {
-// 	fmt.Println(hew([]string{"hello", "world", "welcome", "(cap)", "golang"}))
-// }
-
-// func singleUp(s string) string {
-// 	words := strings.Fields(s)
-// 	for i := 0; i < len(words); i++ {
-// 		if words[i] == "(up)" && i > 0 {
-// 			words[i-1] = strings.ToUpper(words[i-1])
-// 			words = append(words[:i], words[i+1:]...)
-// 		}
-// 	}
-// 	r := strings.Join(words, " ")
-// 	r = strings.ReplaceAll(r, " !", "!")
-// 	return r
-// }
-
-// func uppcase(s string) string {
-// 	words := strings.Fields(s)
-
-// 	for i := 0; i < len(words)-1; i++ {
-// 		switch words[i] {
-// 		case "(up,":
-// 			words[i+1] = strings.Trim(words[i+1], ")")
-// 			num, err := strconv.Atoi(words[i+1])
-// 			if err != nil {
-// 				fmt.Println(err)
-// 			}
-// 			for j := 1; j <= num; j++ {
-// 				words[i-j] = strings.ToUpper(words[i-j])
-// 			}
-// 			words = append(words[:i], words[i+2:]...)
-// 			i--
-// 		}
-// 	}
-// 	return strings.Join(words, " ")
-
-// // }
-
-// func main() {
-// 	fmt.Println(uppcase("This is so exciting (up, 2)"))
-// }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"strings"
-// 	"unicode"
-// 	// "strings"
-// 	// "unicode"
-// )
-
-// func ma(s string) string {
-// 	c := []rune(s)
-// 	var result []rune
-
-// 	for i := 0; i < len(c); i++ {
-// 		if unicode.IsLetter(c[i]) || unicode.IsSpace(c[i]) {
-// 			result = append(result, c[i])
-// 		}
-// 	}
-// 	r := strings.Join(strings.Fields(string(result)), " ")
-// 	return r
-// // }
-
-// func count(s string) string {
-// 	letters := 0
-// 	b := 0
-// 	for _, r := range s {
-// 		if unicode.IsLetter(r) {
-// 			letters++
-// 		}
-// 		if unicode.IsPunct(r) {
-// 			b++
-// 		}
-// 	}
-// 	out := len(strings.Fields(s))
-// 	h := strings.Count(s, " ")
-
-// 	return fmt.Sprint("Number of letters: ", letters, " \n"+"space: ", out, "\n"  + "words: ", h, "\n", "Punct: ", b)
-
-// }
-
-// func main() {
-
-// 	fmt.Println(count(",, hello , ;world ! . , : benjamin ., ,. "))
-// }
-
-// package main
-
-// import (
-// 	"fmt"
 // 	"strings"
 // )
 
