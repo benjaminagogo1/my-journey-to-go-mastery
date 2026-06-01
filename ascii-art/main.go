a74532c775584fdd23d11ff170520a34d3ece027
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-06-01 23:50:37 +0100

    my-journey-to-go-mastery

--- ascii-art/main.go
+++ ascii-art/main.go
@@ -22,3 +22,37 @@ func main() {
 	input := os.Args[1]
 	fmt.Print(asciiArt(input, r))
 }
+
+
+
+
+
+
+package main
+
+import (
+	"fmt"
+)
+
+type studentResult struct {
+		Name string
+		Age int
+		Scores map[string]int{
+			"key1" : value1
+			"key2" : value2
+			"key3" : value3
+		}
+	}
+func studentReport(student string) []string {
+	
+	studentScores := studentResult{
+		Name : "Benjamin",
+		Age : 22,
+		Scores : map[string]int{
+			"Maths" : 85
+			"English" : 70
+			"Chemistry" : 90
+		},
+
+	}
+}
