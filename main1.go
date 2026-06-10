cb5e62c0232eca00df760bf6662c0655f74e25a4
botete <josephbenjamin046@gmail.com>
2026-06-10 20:57:26 +0100

    my-journey-to-g-mastery

--- main1.go
+++ main1.go
@@ -678,3 +678,31 @@ func TestGetLogs_ReturnsAllEntries(t *testing.T) {
 		}
 	}
 }
+
+
+
+package main
+
+import (
+	"fmt"
+)
+
+func RetainFirstHalf(s string) string {
+	if len(s) == 1  {
+		return s
+	}
+	if s == "" {
+		return ""
+	}
+	fmt.Println(len(s))
+	half := len(s)/2
+	return s[:half]
+}
+
+
+func main() {
+	fmt.Println(RetainFirstHalf("This is the 1st halfThis is the 2nd half"))
+	fmt.Println(RetainFirstHalf("A"))
+	fmt.Println(RetainFirstHalf(""))
+	fmt.Println(RetainFirstHalf("Hello World"))
+}
