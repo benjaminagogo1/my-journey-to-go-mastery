120becee199c10d15977d171b1f00da09bb24422
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-06-21 05:54:12 +0100

    HTTP Multiplexer Concept

--- AsciiArt-Web-Exercises/HTTP-Resurgence/v2_Exercise6.go
+++ AsciiArt-Web-Exercises/HTTP-Resurgence/v2_Exercise6.go
@@ -1,1 +1,32 @@
 package main
+
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Exercise6() {
+	mainMux := http.NewServeMux()
+	apiMux := http.NewServeMux()
+
+	mainMux.Handle("/api/", http.StripPrefix("/api", apiMux))
+
+	mainMux.HandleFunc("/api/v1/ping", pingHandle)
+	mainMux.HandleFunc("/api/v1/greet", greetHandle)
+	fmt.Println("Server is running.....")
+	http.ListenAndServe(":8080", mainMux)
+}
+
+func pingHandle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "pong")
+}
+
+func greetHandle(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		fmt.Fprint(w, "Greetings, Stranger!")
+	} else {
+		fmt.Fprintf(w, "Greetings, %v!", name)
+	}
+}
--- AsciiArt-Web-Exercises/HTTP-Resurgence/v2_Exercise7.go
+++ AsciiArt-Web-Exercises/HTTP-Resurgence/v2_Exercise7.go
@@ -1,1 +1,48 @@
 package main
+
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func Exercise7() {
+	http.HandleFunc("/render", renderHandle)
+	fmt.Println("Server running....")
+	http.ListenAndServe(":8080", nil)
+}
+
+const tmplStr = `
+<!DOCTYPE html>
+<html>
+<head><title>{{.Title}}</title></head>
+<body>
+	<h1>{{.Title}}</h1>
+	<p>{{.Body}}</p>
+</body>
+`
+
+type pageData struct {
+	Title string
+	Body  string
+}
+
+func renderHandle(w http.ResponseWriter, r *http.Request) {
+
+	tpl := template.Must(template.New("page").Parse(tmplStr))
+
+	title := r.URL.Query().Get("title")
+	body := r.URL.Query().Get("body")
+	if title == "" || body == "" {
+		http.Error(w, "title and body are required", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	 
+	err := tpl.Execute(w, pageData{Title: title, Body: body})
+	if err != nil {
+		http.Error(w, "template execution failsed", http.StatusInternalServerError)
+		return
+	}
+}
