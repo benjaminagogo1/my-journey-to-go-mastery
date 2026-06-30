174950eea639cf4801ea6431879e3fa588dc0629
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-06-30 22:34:46 +0100

    API Server Call

--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -1,11 +1,45 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
-type Artists struct{
-	ID int
-	Name string
-	Image string
-	Member []string
+type Artists struct {
+	ID           int
+	Name         string
+	Image        string
+	Member       []string
 	CreationDate int
-	FirstAlbum string
+	FirstAlbum   string
 }
+
+func main() {
+	url := "https://groupietrackers.herokuapp.com/api/artists/1"
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	if response.StatusCode == http.StatusOK {
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// data := string(body)
+		// fmt.Println(data)
+		allArtists := Artists{}
+		json.Unmarshal(body, &allArtists)
+		fmt.Println(allArtists)
+
+	}
+}
+
+// {"artists"
+// :"",
+// "locations":"https://groupietrackers.herokuapp.com/api/locations",
+// "dates":"https://groupietrackers.herokuapp.com/api/dates",
+// "relation":"https://groupietrackers.herokuapp.com/api/relation"}
